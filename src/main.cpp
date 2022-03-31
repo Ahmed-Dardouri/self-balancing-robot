14e2ee7785e2db4750731035a1bf44e5d57d85cf
ahmed dardouri <ahmed@dardouri.com>
2022-03-31 13:54:56 +0100

    added gyroscope code, added roll, pitch, yaw calculation raw code

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,14 @@
-//#include "MPU9250.h"
-#include <Arduino.h>
+#include "MPU9250.h"
+void printIMUData();
 // an MPU9250 object with the MPU-9250 sensor on I2C bus 0 with address 0x68
-//MPU9250 IMU(Wire,0x68);
-//int status;
-int i = 0;
+MPU9250 IMU(Wire,0x68);
+int status;
+double roll , pitch, yaw;
 void setup() {
   // serial to display data
   Serial.begin(115200);
-  /*while(!Serial) {}
-  Serial.println("serial mrigl");
+  while(!Serial) {}
+
   // start communication with IMU 
   status = IMU.begin();
   if (status < 0) {
@@ -16,28 +16,76 @@ void setup() {
     Serial.println("Check IMU wiring or try cycling power");
     Serial.print("Status: ");
     Serial.println(status);
-  }else{
-    Serial.println("mriiigl");
-  }*/
+    while(1) {}
+  }
 }
 
 void loop() {
-  if (i == 0){
-    
-    Serial.println("hello from setup");
-    Serial.println("--------------------------");
-    i++;
-  }else{
-    Serial.println("hello from loop");
-    /*IMU.readSensor();
+  // read the sensor
+  IMU.readSensor();
+  printIMUData();
+  delay(500);
+  // display the data
+  /*Serial.print(IMU.getAccelX_mss(),6);
+  Serial.print("\t");
+  Serial.print(IMU.getAccelY_mss(),6);
+  Serial.print("\t");
+  Serial.print(IMU.getAccelZ_mss(),6);
+  Serial.print("\t");
+  Serial.print(IMU.getGyroX_rads(),6);
+  Serial.print("\t");
+  Serial.print(IMU.getGyroY_rads(),6);
+  Serial.print("\t");
+  Serial.print(IMU.getGyroZ_rads(),6);
+  Serial.print("\t");
+  Serial.print(IMU.getMagX_uT(),6);
+  Serial.print("\t");
+  Serial.print(IMU.getMagY_uT(),6);
+  Serial.print("\t");
+  Serial.print(IMU.getMagZ_uT(),6);
+  Serial.print("\t");
+  Serial.println(IMU.getTemperature_C(),6);
+  delay(100);*/
 
-    Serial.print(IMU.getGyroX_rads(),2);
-    Serial.print("\t");
-    Serial.print(IMU.getGyroY_rads(),2);
-    Serial.print("\t");
-    Serial.println(IMU.getGyroZ_rads(),2);
-    Serial.println("______________________________");*/
-    delay(1000);
-  }
+}
+
+void printIMUData(){  
   
+  float accelX = IMU.getAccelX_mss();
+  float accelY = IMU.getAccelY_mss();
+  float accelZ = IMU.getAccelZ_mss();
+  float gyroX = IMU.getGyroX_rads()/57.3;
+  float gyroY = IMU.getGyroY_rads()/57.3;
+  float gyroZ = IMU.getGyroZ_rads()/57.3;
+  float magX = IMU.getMagX_uT();
+  float magY = IMU.getMagY_uT();
+  float magZ = IMU.getMagZ_uT();
+
+  Serial.println("Accel: " + String(accelX) + ", " + String(accelY) + ", " + String(accelZ) + " g");
+  Serial.println("Gyro: " + String(gyroX) + ", " + String(gyroY) + ", " + String(gyroZ) + " dps");
+  Serial.println("Mag: " + String(magX) + ", " + String(magY) + ", " + String(magZ) + " uT");
+
+//Euler angle from accel
+
+ 
+  pitch = atan2 (accelY ,( sqrt ((accelX * accelX) + (accelZ * accelZ))));
+  roll = atan2(-accelX ,( sqrt((accelY * accelY) + (accelZ * accelZ))));
+
+   // yaw from mag
+
+  float Yh = (magY * cos(roll)) - (magZ * sin(roll));
+  float Xh = (magX * cos(pitch))+(magY * sin(roll)*sin(pitch)) + (magZ * cos(roll) * sin(pitch));
+
+  yaw =  atan2(Yh, Xh);
+
+
+  roll = roll*57.3;
+  pitch = pitch*57.3;
+  yaw = yaw*57.3;
+   
+  Serial.println("pitch"  + String( pitch) );
+  Serial.println("roll" + String( roll));
+  Serial.println("yaw" + String( yaw ));
+
+
 }
